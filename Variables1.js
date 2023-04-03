eb9014d7137f35650da08474ce4e4f092fb4c7a6
Antonio Gennaro <antgennaro@hotmail.com>
2023-04-03 17:41:00 -0300

    practica 1.6 parte 1 v2

--- Variables1.js
+++ Variables1.js
@@ -2,7 +2,7 @@
 let nombreIngresado = "pepe"; // nombre genérico 
 nombreIngresado = prompt("Escribe tu nombre"); // pido el nombre
 document.querySelector("body").innerHTML = "El nombre ingresado es:  " 
-    + nombreIngresado + "\r\n"; // lo imprimo
-document.write("\r\nHola " + nombreIngresado + " Alguna otra Cosa?");
+    + nombreIngresado + "\r \n"; // lo imprimo
+document.write("\r \n Hola " + nombreIngresado + " Alguna otra Cosa?");
 
 
