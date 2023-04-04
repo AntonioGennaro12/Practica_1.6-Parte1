dd17f63b4399de44ac503ab47f9e6265b78d2f02
Antonio Gennaro <antgennaro@hotmail.com>
2023-04-04 10:54:02 -0300

    Práctica 6 parte 2

--- Variables1.js
+++ Variables1.js
@@ -1,9 +1,30 @@
-
-let nombreIngresado = "pepe"; // nombre genérico 
-nombreIngresado = prompt("Escribe tu nombre"); // pido el nombre
+// Esta es la parte 2
+// Defino todas las variables a utilizar 
+let caracterIngresado      = "x";      // un caracter genérico  
+let stringIngresado        = "x123*";  // un string cualquiera
+let enteroIngresado        = 999999;   // un entero cualquiera  
+let puntoflotanteIngresado = 999.99;   // un numero con decimales 
+let booleanoIngresado      = false ;   // boolean "True" o "False"
+// Pido todos los valores para c/u de las variables  
+caracterIngresado = prompt("Escribe un Caracter"); // pido el char
+stringIngresado = prompt("Escribe una Cadena de Caracteres"); // pido el String
+enteroIngresado = prompt("Escribe un Número Entero"); // pido el Integer
+puntoflotanteIngresado = prompt("Escribe un Número con Decimales"); // pido el float
+booleanoIngresado = prompt(`Escribe un Boolean "True" o "False"`); // pido un boolean
+// Ahora los muestro a todos en H2
+document.write("<h1><h1>"); // defino tipo de letra e inserto linea
+document.write("Muestro los datos ingresados");
 document.write("<h2><h2>"); // defino tipo de letra
-document.write("El nombre ingresado es:  " + nombreIngresado );
+document.write("El Caracter ingresado es:   " + caracterIngresado );
+document.write("<h2><h2>"); // defino tipo de letra
+document.write("El String ingresado es:  " + stringIngresado );
+document.write("<h2><h2>"); // defino tipo de letra
+document.write("El Entero ingresado es:  " + enteroIngresado );
+document.write("<h2><h2>"); // defino tipo de letra
+document.write("El Núm. con Decimales ingresado es:  " + puntoflotanteIngresado );
+document.write("<h2><h2>"); // defino tipo de letra
+document.write("El Booleano ingresado es:  " + booleanoIngresado );
 document.write("<h3><h3>"); // defino tipo de letra e inserto linea
-document.write("Hola " + nombreIngresado + " Alguna otra Cosa?");
+document.write("Fin");
 
 
